f3d62872fe441a4f373e7f20825a22339edbb4aa
Mohamed Adel Ebrahim <medoelbob11@gmail.com>
2022-07-31 18:04:34 +0200

    Added session 1

---
+++ Lec1/Labs/lab1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main(){
+ printf("Iam Mohamed\n My birthday is 2 jul 2001\n I graduate from fuculty of engineering \n mansoura university 2024");
+ 
+}
---
+++ Lec1/assignments/Assignment3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+ int x,y;
+ printf("please enter number1 :");
+ scanf(" %d" , &x);
+ printf("please enter number2 :");
+ scanf(" %d" , &y);
+ printf("a+b = %d\n", (x+y));
+ printf("a-b = %d\n", (x-y));
+ printf("a&b = %d\n", (x&y));
+ printf("a|b = %d\n", (x|y));
+ printf("a^b = %d\n", (x^y));
+ 
+ 
+}
